60c6aa70abc1634b2aae54973eeab01c2b0685f6
Kent Merrell <kent.merrell@nih.gov>
2023-02-20 19:49:47 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -55,31 +55,66 @@ const data = [
   }
 ];
 
-// Find the visualization container element
 const visualization = document.getElementById("visualization");
 
-// Loop through the data and create a column for each skill
-data.forEach((skillData) => {
-  const skill = skillData.skill;
-  const years = Object.keys(skillData).filter((key) => key !== "skill");
-  const column = document.createElement("div");
-  column.className = "column";
+function createVisualization(data, startYear, endYear) {
+  // Remove any columns that are already in the visualization
+  console.log("starting");
+  while (visualization.firstChild) {
+    visualization.removeChild(visualization.firstChild);
+  }
 
-  // Loop through the years and create a box for each instance of the skill
-  years.forEach((year) => {
-    const count = skillData[year];
-    for (let i = 0; i < count; i++) {
-      const box = document.createElement("div");
-      box.className = "box";
-      column.appendChild(box);
-    }
+  // Filter the data to include only the years within the given range
+  const filteredData = data.map((skillData) => {
+    const filteredYears = Object.keys(skillData).filter(
+      (key) =>
+        key !== "skill" &&
+        parseInt(key) >= startYear &&
+        parseInt(key) <= endYear
+    );
+    const filteredSkillData = { skill: skillData.skill };
+    filteredYears.forEach((year) => {
+      filteredSkillData[year] = skillData[year];
+    });
+
+    return filteredSkillData;
   });
 
-  // Add the skill name to the column
-  const label = document.createElement("div");
-  label.textContent = skill;
-  column.appendChild(label);
+  // Loop through the filtered data and create a column for each skill
+  console.log(filteredData);
+  filteredData.forEach((skillData) => {
+    const skill = skillData.skill;
+    const years = Object.keys(skillData).filter((key) => key !== "skill");
+    const column = document.createElement("div");
+    column.className = "column";
 
-  // Add the column to the visualization container
-  visualization.appendChild(column);
-});
+    // Loop through the years and create a box for each instance of the skill
+    years.forEach((year) => {
+      const count = skillData[year];
+      for (let i = 0; i < count; i++) {
+        const box = document.createElement("div");
+        box.className = "box";
+        column.appendChild(box);
+      }
+    });
+
+    // Add the skill name to the column
+    const label = document.createElement("div");
+    label.textContent = skill;
+    column.appendChild(label);
+
+    // Add the column to the visualization container
+    visualization.appendChild(column);
+  });
+}
+
+// Initial visualization with all years
+createVisualization(data, 2015, 2019);
+
+// Add event listener to the range input to update visualization on change
+// const rangeInput = document.getElementById('range');
+// rangeInput.addEventListener('input', (event) => {
+//   const startYear = parseInt(rangeInput.value.split(',')[0]);
+//   const endYear = parseInt(rangeInput.value.split(',')[1]);
+//   createVisualization(data, startYear, endYear);
+// });
